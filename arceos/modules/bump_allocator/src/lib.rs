794ae2436d4f42792169cd528f5099dd953880fc
Kirikaze Chiyuki <me@chyk.ink>
2026-04-28 09:45:19 +0800

    feat: simple bump allocator (multiple regions and dealloc not supported)

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -16,73 +16,141 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const PAGE_SIZE: usize> {
+    start: usize,
+    end: usize,
+    b_pos: usize,
+    p_pos: usize,
+    count: usize,
+}
 
-impl<const SIZE: usize> EarlyAllocator<SIZE> {
+impl<const PAGE_SIZE: usize> EarlyAllocator<PAGE_SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            end: 0,
+            b_pos: 0,
+            p_pos: 0,
+            count: 0,
+        }
     }
 }
 
-impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
+impl<const PAGE_SIZE: usize> BaseAllocator for EarlyAllocator<PAGE_SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.start = start;
+        self.end = start + size;
+        self.b_pos = start;
+        self.p_pos = start + size;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
-        todo!()
+        unimplemented!()
     }
 }
 
-impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
+fn align_up_addr(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
+
+fn align_down_addr(addr: usize, align: usize) -> usize {
+    addr & !(align - 1)
+}
+
+impl<const PAGE_SIZE: usize> ByteAllocator for EarlyAllocator<PAGE_SIZE> {
     fn alloc(
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        let alloc_start = align_up_addr(self.b_pos, layout.align());
+
+        // 检查是否有足够的空间进行分配
+        let alloc_end = alloc_start
+            .checked_add(layout.size())
+            .ok_or(allocator::AllocError::NoMemory)?;
+        if alloc_end > self.p_pos {
+            return Err(allocator::AllocError::NoMemory);
+        }
+
+        let slice = unsafe { core::slice::from_raw_parts_mut(alloc_start as *mut u8, layout.size()) };
+        slice.fill(0);
+
+        self.b_pos = alloc_end;
+        self.count += 1;
+
+        return Ok(core::ptr::NonNull::new(alloc_start as *mut u8).unwrap());
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        let alloc_start = pos.as_ptr() as usize;
+        let alloc_end = align_up_addr(alloc_start, layout.align())
+            .checked_add(layout.size())
+            .expect("Overflow in dealloc");
+
+        if alloc_start < self.start || alloc_end > self.b_pos {
+            panic!("Out of bounds");
+        }
+
+        self.count -= 1;
+        if self.count == 0 {
+            let slice = unsafe {
+                core::slice::from_raw_parts_mut(self.start as *mut u8, self.b_pos - self.start)
+            };
+            slice.fill(0);
+            // reset b_pos
+            self.b_pos = self.start;
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!()
+        self.p_pos - self.start
     }
 
     fn used_bytes(&self) -> usize {
-        todo!()
+        self.b_pos - self.start
     }
 
     fn available_bytes(&self) -> usize {
-        todo!()
+        self.p_pos - self.b_pos
     }
 }
 
-impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
-    const PAGE_SIZE: usize = SIZE;
+impl<const PAGE_SIZE: usize> PageAllocator for EarlyAllocator<PAGE_SIZE> {
+    const PAGE_SIZE: usize = PAGE_SIZE;
 
     fn alloc_pages(
         &mut self,
         num_pages: usize,
-        align_pow2: usize,
+        align_pow2: usize, // must be a power of 2 and multiple of PAGE_SIZE
     ) -> allocator::AllocResult<usize> {
-        todo!()
+        if align_pow2 % Self::PAGE_SIZE != 0 || !align_pow2.is_power_of_two() {
+            return Err(allocator::AllocError::InvalidParam);
+        }
+        let align_start = align_down_addr(self.p_pos - num_pages * Self::PAGE_SIZE, align_pow2);
+        if align_start < self.b_pos {
+            return Err(allocator::AllocError::NoMemory);
+        }
+        let size = num_pages * Self::PAGE_SIZE;
+        let slice = unsafe { core::slice::from_raw_parts_mut(align_start as *mut u8, size) };
+        slice.fill(0);
+        self.p_pos = align_start;
+        // 返回结果是 align_start
+        Ok(align_start)
     }
 
-    fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
-        todo!()
+    fn dealloc_pages(&mut self, _pos: usize, _num_pages: usize) {
+        unimplemented!()
     }
 
     fn total_pages(&self) -> usize {
-        todo!()
+        self.end - self.b_pos / Self::PAGE_SIZE
     }
 
     fn used_pages(&self) -> usize {
-        todo!()
+        self.end - self.p_pos / Self::PAGE_SIZE
     }
 
     fn available_pages(&self) -> usize {
-        todo!()
+        self.total_pages() - self.used_pages()
     }
 }
